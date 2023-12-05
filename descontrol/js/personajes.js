ac298c7f1da7f7d51e010ead74020617fc8b40fb
devfer <devfercastro@gmail.com>
2023-12-05 20:15:36 -0300

    simplificación de acceso a personajes

--- descontrol/js/personajes.js
+++ descontrol/js/personajes.js
@@ -1,96 +1,101 @@
-class Personaje {
-    constructor({
-        nombre = "",
-        descripcion = "",
-        imagen = "",
-        ataque = 0,
-        esquiva = 0,
-        velocidad = 0,
-        vida = 0,
-        accion = 0,
-    }) {
-        this.nombre = nombre
-        this.descripcion = descripcion
-        this.imagen = imagen
-        this.ataque = ataque
-        this.esquiva = esquiva
-        this.velocidad = velocidad
-        this.vida = vida
-        this.accion = accion
-    }
-
-    modificarAtributos(obj) {
-        for (const key in obj.general) {
-            this[key] = obj[key]
-        }
-        for (const key in obj.atributos) {
-            if (key in this) {
-                this[key] += obj[key]
-            }
-        }
-    }
-}
-
 const personajesDict = {
     "default": {
-        general: {
-            nombre: "default",
-            descripcion: "personaje sin descripción",
-            imagen: "img/nuevopjico.png",
-        },
-        atributos: {
-            ataque: 0,
-            esquiva: 0,
-            velocidad: 0,
-            vida: 0,
-            accion: 0,
-        },
+        nombre: "default",
+        descripcion: "personaje sin descripción",
+        imagen: "img/nuevopjico.png",
+        ataque: 0,
+        esquiva: 0,
+        velocidad: 0,
+        vida: 0,
+        accion: 0,
     },
-    "test 1": {
-        general: {
-            nombre: "test 1",
-            descripcion: "descripcion test 1",
-            imagen: "img/nuevopjico.png",
-        },
-        atributos: {
-            ataque: 6,
-            esquiva: 6,
-            velocidad: 6,
-            vida: 18,
-            accion: 6,
-        },
+    "test1": {
+        nombre: "test1",
+        descripcion: "descripcion test1",
+        imagen: "img/nuevopjico.png",
+        ataque: 6,
+        esquiva: 6,
+        velocidad: 6,
+        vida: 18,
+        accion: 6,
     },
-    "test 2": {
-        general: {
-            nombre: "test 2",
-            descripcion: "descripcion test 2",
-            imagen: "img/nada.png",
-        },
-        atributos: {
-            ataque: 3,
-            esquiva: 3,
-            velocidad: 3,
-            vida: 9,
-            accion: 3,
-        },
+    "test2": {
+        nombre: "test2",
+        descripcion: "descripcion test2",
+        imagen: "img/nuevopjico.png",
+        ataque: 10,
+        esquiva: 10,
+        velocidad: 10,
+        vida: 20,
+        accion: 10,
     },
-    "test 3": {
-        general: {
-            nombre: "test 3",
-            descripcion: "descripcion test 3",
-            imagen: "img/nada.png",
-        },
-        atributos: {
-            ataque: 3,
-            esquiva: 3,
-            velocidad: 3,
-            vida: 9,
-            accion: 3,
-        },
-    },
+    "test3": {
+        nombre: "test3",
+        descripcion: "descripcion test3",
+        imagen: "img/nuevopjico.png",
+        ataque: 12,
+        esquiva: 12,
+        velocidad: 12,
+        vida: 22,
+        accion: 12,
+    }
 }
 
-let listaPersonajes = []
-for (const key in personajesDict) {
-    if (key != "default") listaPersonajes.push(new Personaje({ ...personajesDict[key].general, ...personajesDict[key].atributos }))
-}
+// const personajesDict = {
+//     "default": {
+//         general: {
+//             nombre: "default",
+//             descripcion: "personaje sin descripción",
+//             imagen: "img/nuevopjico.png",
+//         },
+//         atributos: {
+//             ataque: 0,
+//             esquiva: 0,
+//             velocidad: 0,
+//             vida: 0,
+//             accion: 0,
+//         },
+//     },
+//     "test1": {
+//         general: {
+//             nombre: "test1",
+//             descripcion: "descripcion test1",
+//             imagen: "img/nuevopjico.png",
+//         },
+//         atributos: {
+//             ataque: 6,
+//             esquiva: 6,
+//             velocidad: 6,
+//             vida: 18,
+//             accion: 6,
+//         },
+//     },
+//     "test2": {
+//         general: {
+//             nombre: "test2",
+//             descripcion: "descripcion test2",
+//             imagen: "img/nada.png",
+//         },
+//         atributos: {
+//             ataque: 3,
+//             esquiva: 3,
+//             velocidad: 3,
+//             vida: 9,
+//             accion: 3,
+//         },
+//     },
+//     "test3": {
+//         general: {
+//             nombre: "test3",
+//             descripcion: "descripcion test3",
+//             imagen: "img/nada.png",
+//         },
+//         atributos: {
+//             ataque: 3,
+//             esquiva: 3,
+//             velocidad: 3,
+//             vida: 9,
+//             accion: 3,
+//         },
+//     },
+// }
