19874d5c96a95726b4fc83687592f710a4409c71
devfer <devfercastro@gmail.com>
2023-12-11 22:23:32 -0300

    Cartas de prueba

---
+++ descontrol/js/cartas.js
@@ -0,0 +1,38 @@
+const cartasNormalesDict = {
+    "carta normal 1": {
+        nombre: "carta normal 1",
+        descripcion: "sin descripcion",
+        imagen: "img/new.png",
+        daño: 2,
+        esquiva: 2,
+        velocidad: 2,
+        vida: -1,
+        accion: 2,
+    }
+}
+
+const cartasEspecialesDict = {
+    "carta especial 1": {
+        nombre: "carta especial 1",
+        descripcion: "sin descripcion",
+        imagen: "img/new.png",
+        daño: 2,
+        esquiva: 2,
+        velocidad: 2,
+        vida: -1,
+        accion: 2,
+    }
+}
+
+const cartasEpicasDict = {
+    "carta epica 1": {
+        nombre: "carta epica 1",
+        descripcion: "sin descripcion",
+        imagen: "img/new.png",
+        daño: 2,
+        esquiva: 2,
+        velocidad: 2,
+        vida: -1,
+        accion: 2,
+    }
+}
